084c4e3293d5028acddba122c9931ff457708dbd
Swarnim Dixit <swarnim1234@gmail.com>
2017-03-28 13:50:43 +0530

    final

--- app/src/main/java/com/example/trigent/markmartiak/ContactFragment.java
+++ app/src/main/java/com/example/trigent/markmartiak/ContactFragment.java
@@ -1,6 +1,10 @@
 package com.example.trigent.markmartiak;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,11 +18,15 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ContactFragment extends Fragment {
 
 ImageButton IBFaceBook,IBTwitter,IBLinkedIn;
+
+    public static String FACEBOOK_URL = "https://www.facebook.com/premieradvisormmartiak";
+    public static String FACEBOOK_PAGE_ID = "premieradvisormmartiak";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,10 +37,68 @@ ImageButton IBFaceBook,IBTwitter,IBLinkedIn;
         IBLinkedIn = (ImageButton) view.findViewById(R.id.contact_frag_ib_linkedin);
 
 
+        IBFaceBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+                Intent facebookIntent = new Intent(Intent.ACTION_VIEW);
+                String facebookUrl = getFacebookPageURL(getContext());
+                facebookIntent.setData(Uri.parse(facebookUrl));
+                startActivity(facebookIntent);
+
+            }
+        });
+
+
+        IBTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("twitter://user?user_id=511026754"));
+                    startActivity(intent);
+
+                }catch (Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://twitter.com/#!/[premieradvisor]")));
+                }
+            }
+        });
+
+        IBLinkedIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("linkedin://premieradvisor"));
+                final PackageManager packageManager = getContext().getPackageManager();
+                final List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+                if (list.isEmpty()) {
+                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/premieradvisor"));
+                }
+                startActivity(intent);
+            }
+        });
+
+
+
+
+        return view;
     }
 
+
+
+    public String getFacebookPageURL(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            int versionCode = packageManager.getPackageInfo("com.facebook.katana", 0).versionCode;
+            if (versionCode >= 3002850) { //newer versions of fb app
+                return "fb://facewebmodal/f?href=" + FACEBOOK_URL;
+            } else { //older versions of fb app
+                return "fb://page/" + FACEBOOK_PAGE_ID;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            return FACEBOOK_URL; //normal web url
+        }
+    }
 
 
 }
